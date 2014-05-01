11286af152704e7439128663ef165280b19c6178
zhangjie <luoyetx@gmail.com>
2014-05-01 14:37:19 +0800

    climbing stairs

---
+++ code/climbing_stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+  # @param n, an integer
+  # @return an integer
+  def climbStairs(self, n):
+    x = y = 1
+    while n > 1:
+      temp = x + y
+      x = y
+      y = temp
+      n = n - 1
+    return y
+
+
+if __name__ == '__main__':
+  sol = Solution()
+  print sol.climbStairs(3)
