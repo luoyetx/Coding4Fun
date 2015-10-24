20900b2504359dbef0c06054000416c1968e79ad
zhangjie <luoyetx@gmail.com>
2015-10-24 14:37:56 +0800

    optimaze

--- leetcode/maximum_product_subarray.c
+++ leetcode/maximum_product_subarray.c
@@ -5,20 +5,15 @@
 #define min(x, y) ((x)<(y)?(x):(y))
 
 int maxProduct(int* nums, int numsSize) {
-    int* f = (int*)malloc(numsSize*sizeof(int));
-    int* g = (int*)malloc(numsSize*sizeof(int));
-
-    int m;
-    m = f[0] = g[0] = nums[0];
-
-    int i;
+    int i, m, f, g;
+    m = f = g = nums[0];
     for (i=1; i<numsSize; i++) {
-        int a = f[i-1]*nums[i];
-        int b = g[i-1]*nums[i];
+        int a = f*nums[i];
+        int b = g*nums[i];
         int c = nums[i];
-        f[i] = max(max(a, b), c);
-        g[i] = min(min(a, b), c);
-        if (f[i] > m) m = f[i];
+        f = max(max(a, b), c);
+        g = min(min(a, b), c);
+        if (f > m) m = f;
     }
     return m;
 }
